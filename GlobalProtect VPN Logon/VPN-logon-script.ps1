3aca2358f3b694e2d542e9cd089055fa026fe231
Sjos5065 <41280079+Sjos5065@users.noreply.github.com>
2021-06-23 16:28:26 +0530

    Add files via upload

---
+++ GlobalProtect VPN Logon/VPN-logon-script.ps1
@@ -0,0 +1,34 @@
+# Copy the policy update batch file from network to local machine
+
+$source = "\\IVGSS-DC-SCV01\Scripts\policy_update.bat"
+
+$tempdirexists = Test-Path -Path "c:\Temp"
+
+$destination = "C:\Temp"
+
+if ($tempdirexists){
+    Copy-Item -Path $source -Destination $destination
+}
+else{
+    New-Item -ItemType directory -Path $destination
+    Copy-Item -Path $source -Destination $destination
+}
+
+# Check for post vpn connect regsitry on the machine, if it does not exist, create it.
+
+$pathexist = Test-Path -Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\Settings\post-vpn-connect"
+
+if ($pathexist){
+    $regitems = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\Settings\post-vpn-connect"
+
+    if ("command" -notin ($regitems)){
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\Settings\post-vpn-connect" -Name command -value "C:\Temp\Policy_update.bat > log  2> error"
+    }
+    else{
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\Settings\post-vpn-connect" -Name command -value "C:\Temp\Policy_update.bat > log  2> error"
+    }
+}
+else{
+    New-Item -Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\Settings" -Name post-vpn-connect
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\Settings\post-vpn-connect" -Name command -value "C:\Temp\Policy_update.bat > log  2> error"
+}
